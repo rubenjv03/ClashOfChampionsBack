4eb366f69d158d48b738e12e0ff4a167265e21a4
Rubén Jorge Verdeguer <st2021378@365.stucom.com>
2023-05-26 19:19:05 +0200

    migraciones cambiadas

--- migrations/Version20230522154602.php
+++ migrations/Version20230522154602.php
@@ -20,7 +20,7 @@ final class Version20230522154602 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE tournament (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, part_num INT NOT NULL, game_name VARCHAR(255) NOT NULL, date_begin DATETIME NOT NULL, date_end DATETIME NOT NULL, is_active BOOL NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE tournament (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, part_num INT NOT NULL, game_name VARCHAR(255) NOT NULL, date_begin DATETIME NOT NULL, date_end DATETIME NOT NULL, is_active BOOL NOT NULL, PRIMARY KEY(id), FOREIGN KEY(game_name) REFERENCES game(game_name))) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void
