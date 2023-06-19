3f2159ad85c46ccb5ad37cf90debf0c4061a0968
stevenCevallos <114929330+stevenCevallos@users.noreply.github.com>
2023-06-19 19:00:28 +0200

    usuarios-match se registran

--- src/Controller/MatchController.php
+++ src/Controller/MatchController.php
@@ -5,6 +5,7 @@ use App\Entity\Tournament;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\MatchGame;
 use Symfony\Component\HttpFoundation\Session\Session;
@@ -43,13 +44,21 @@ class MatchController extends AbstractController
         $entityManager = $doctrine->getManager();
         $repository = $entityManager->getRepository(MatchGame::class);
         $match = $repository->findOneBy(array('tournament_id' => $tournament));
-        if($match->getParticipant1Name() != null){
-            $match->setParticipant2Name($session->get('name'));
-        }else{
-            $match->setParticipant1Name($session->get('name'));
+            if($match->getParticipant1Name() == null){
+                $match->setParticipant1Name($session->get('nickname'));
+            }else{
+                $match->setParticipant2Name($session->get('nickname'));
+            }
+            try {
+                $entityManager->flush();
+            } catch (\Exception $e) {
+                // Aquí puedes manejar la excepción, por ejemplo, registrándola
+                error_log($e->getMessage());
+            }
+            return $this->json([
+                'redirectTo' => 'http://localhost:4200/match/id'
+            ]);
         }
-        return $this->json([
-            'redirectTo' => 'http://localhost:4200/match/id'
-        ])
+        
     }
-}
+
